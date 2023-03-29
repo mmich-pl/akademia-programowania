b383007f852c79abc9fcd86edf327f028ccda149
mmich-pl <m.michal.parzych@gmail.com>
2023-03-29 15:52:57 +0200

    solution to reddit task

--- Golang/zadania/reddit/fetcher/redditFetcher.go
+++ Golang/zadania/reddit/fetcher/redditFetcher.go
@@ -1,7 +1,16 @@
 package fetcher
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/sync/errgroup"
 	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 type response struct {
@@ -16,6 +25,93 @@ type response struct {
 }
 
 type RedditFetcher interface {
-	Fetch() error
+	Fetch(ctx context.Context) error
 	Save(io.Writer) error
+}
+
+type Fetcher struct {
+	Address        string
+	response       response
+	mutex          sync.Mutex
+	postCounter    int
+	responseLength int
+}
+
+func (f *Fetcher) SaveAsync(ctx context.Context, path string, limit int) (err error) {
+	f.responseLength = len(f.response.Data.Children)
+	f.postCounter = 0
+
+	group, ctx := errgroup.WithContext(ctx)
+
+	for j := 0; j < limit; j++ {
+		group.Go(func() error {
+			file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+			if err != nil {
+				return fmt.Errorf("cannot save data to file %s: %v", path, err)
+			}
+
+			defer func(file *os.File) {
+				err = file.Close()
+			}(file)
+
+			for {
+				f.mutex.Lock()
+				if f.postCounter < f.responseLength-1 {
+					f.postCounter++
+				} else {
+					f.mutex.Unlock()
+					break
+				}
+				f.mutex.Unlock()
+				post := f.response.Data.Children[f.postCounter]
+				line := fmt.Sprintf("%s\n%s\n", post.Data.Title, post.Data.URL)
+				if _, err := file.Write([]byte(line)); err != nil {
+					log.Printf("cannot write to file: %v", err)
+				}
+			}
+			return nil
+		})
+	}
+
+	return group.Wait()
+}
+
+func (f *Fetcher) Save(file io.Writer) error {
+	for _, post := range f.response.Data.Children {
+		line := fmt.Sprintf("%s\n%s\n", post.Data.Title, post.Data.URL)
+		if _, err := file.Write([]byte(line)); err != nil {
+			return fmt.Errorf("cannot write to file: %v", err)
+		}
+	}
+	return nil
+}
+
+func (f *Fetcher) Fetch(ctx context.Context) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, f.Address, nil)
+	if err != nil {
+		return fmt.Errorf("cannot create request: %v", err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("cannot fetch data: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("status code other than 200: %v", resp.StatusCode)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&f.response); err != nil {
+		return fmt.Errorf("error while decoding data: %v", err)
+	}
+
+	return nil
 }
--- Golang/zadania/reddit/main.go
+++ Golang/zadania/reddit/main.go
@@ -1,9 +1,79 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+	"reddit_fetcher/fetcher"
+	"sync"
 )
 
+type fetcherTuple struct {
+	url, path string
+}
+
 func main() {
-	fmt.Println("hello r/golang!")
+	outputs := []fetcherTuple{
+		{"https://www.reddit.com/r/golang.json?limit=100", "golang.txt"},
+		{"https://www.reddit.com/r/minecraft.json?limit=100", "minecraft.txt"},
+		{"https://www.reddit.com/r/rust.json?limit=100", "rust.txt"},
+	}
+
+	var group sync.WaitGroup
+	group.Add(len(outputs))
+
+	for _, o := range outputs {
+		go execFetcherAsync(o.url, o.path, &group)
+	}
+	group.Wait()
+	log.Println("Task ended")
+}
+
+func execFetcher(url string, path string, group *sync.WaitGroup) {
+	defer group.Done()
+
+	f := fetcher.Fetcher{Address: url}
+
+	if err := f.Fetch(context.Background()); err != nil {
+		log.Println(err)
+	}
+
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+
+	if err != nil {
+		log.Printf("cannot open file '%s': %v\n", path, err)
+	}
+
+	if err := f.Save(file); err != nil {
+		log.Printf("cannot save data: %v\n", err)
+	}
+
+	defer func(file *os.File) {
+		if err := file.Close(); err != nil {
+			log.Printf("cannot close file '%s': %v\n", path, err)
+		}
+	}(file)
+
+	log.Printf("Data saved to file %s", path)
+}
+
+func execFetcherAsync(url string, path string, group *sync.WaitGroup) {
+	if err := os.Remove(path); err != nil {
+		log.Printf("cannot remove file %s", path)
+	}
+
+	defer group.Done()
+	ctx := context.Background()
+
+	f := fetcher.Fetcher{Address: url}
+
+	if err := f.Fetch(ctx); err != nil {
+		log.Println(err)
+	}
+
+	if err := f.SaveAsync(ctx, path, 3); err != nil {
+		log.Printf("cannot save data: %v\n", err)
+	}
+
+	log.Printf("Data saved to file %s", path)
 }
